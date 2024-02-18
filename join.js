d1af51e7b08575549a4b8df1ad6eab0a7bc67717
Mikenziokah <mikenziokah@gmail.com>
2024-02-18 17:37:54 +0300

    changes

--- join.js
+++ join.js
@@ -1,31 +1,30 @@
-// scripts.js
+
 
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('joinNowForm');
 
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent default form submission
+        event.preventDefault();
 
-        // Get form data
+        
         const formData = new FormData(form);
         const name = formData.get('name');
         const email = formData.get('email');
-        const mobile = formData.get('mobile'); // New: Get mobile number field value
-        const password = formData.get('password');
+        const mobile = formData.get('mobile');
+	const content = formData.get('content');
 
-        // Perform form validation (you can add more validation logic here)
-        if (!name || !email || !mobile || !password) { // New: Check if mobile field is empty
+        
+        if (!name || !email || !mobile) {
             alert('Please fill in all fields');
             return;
         }
 
-        // Send form data to the server (you can make an AJAX request here)
+        
         console.log('Name:', name);
         console.log('Email:', email);
-        console.log('Mobile:', mobile); // New: Log mobile number field value
-        console.log('Password:', password);
-
-        // Optionally, reset the form after submission
+        console.log('Mobile:', mobile);
+	console.log('content:', content);
+        
         form.reset();
     });
 });
